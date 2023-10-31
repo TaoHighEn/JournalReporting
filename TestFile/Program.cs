9a8b38a0b29e0dd3844de47297f187e230106182
Alan <32477198+TaoHighEn@users.noreply.github.com>
2023-10-31 14:11:41 +0800

    update testfile

--- TestFile/Program.cs
+++ TestFile/Program.cs
@@ -21,10 +21,16 @@ namespace TestFile
                 using (FileStream fs = File.Open(file.FullName, FileMode.Open, FileAccess.ReadWrite))
                 {
                     work = new XSSFWorkbook(fs);
-                    string s_name = work.GetSheetAt(work.NumberOfSheets - 1).SheetName;
-                    if (s_name == "Evaluation Warning")
+                    if (work.NumberOfSheets != 1)
                     {
-                        work.RemoveSheetAt(work.NumberOfSheets - 1);
+                        for (int i = (work.NumberOfSheets); i > 1; i--)
+                        {
+                            string s_name = work.GetSheetAt(i - 1).SheetName;
+                            if (s_name != "工作表1")
+                            {
+                                work.RemoveSheetAt(i-1);
+                            }
+                        }
                         using (FileStream fs2 = new FileStream(file.FullName, FileMode.Create, FileAccess.Write))
                         {
                             work.Write(fs2);
